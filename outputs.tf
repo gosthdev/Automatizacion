de85cac813e0b1bd3b300031326315faca8bb194
eddu <azabacheeduardo85@gmail.com>
2026-05-24 10:46:49 -0500

    feat: add Cognito user pool ID and client ID outputs in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-
 output "aws_access_key_id" {
   value       = aws_iam_access_key.rekognition_key.id
   description = "Access Key ID para configurar en n8n"
@@ -14,4 +13,13 @@ output "aws_secret_access_key" {
 output "rekognition_collection_arn" {
   value       = aws_rekognition_collection.asistencia.arn
   description = "ARN de la colección de Rekognition creada"
+}
+
+output "cognito_user_pool_id" {
+  value       = aws_cognito_user_pool.pool.id
+  description = "ID del User Pool de Cognito"
+}
+output "cognito_pool_client_id" {
+  value       = aws_cognito_user_pool_client.frontend_client.id
+  description = "ID del User Pool de Cognito"
 }

fe8757401748d8dc2794848ccb4751c9d7aa5171
eddu <azabacheeduardo85@gmail.com>
2026-05-24 08:49:17 -0500

    feat: remove AWS Rekognition and IAM resources from main.tf

--- main.tf
+++ main.tf
@@ -11,28 +11,3 @@ provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
 }
-
-resource "aws_rekognition_collection" "asistencia" {
-  collection_id = var.rekognition_collection_id
-  tags = {
-    Name = "AsistenciaBiometrica"
-  }
-}
-
-resource "aws_iam_user" "n8n_rekognition_user" {
-  name = "n8n-rekognition-service-user"
-  path = "/service-accounts/"
-}
-
-resource "aws_iam_access_key" "n8n_rekognition_key" {
-  user = aws_iam_user.n8n_rekognition_user.name
-}
-
-data "aws_iam_policy" "rekognition_full_access" {
-  name = "AmazonRekognitionFullAccess"
-}
-
-resource "aws_iam_user_policy_attachment" "n8n_rekognition_attach" {
-  user       = aws_iam_user.n8n_rekognition_user.name
-  policy_arn = data.aws_iam_policy.rekognition_full_access.arn
-}

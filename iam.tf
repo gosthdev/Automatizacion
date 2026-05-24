0b20edc9848a1b2784e3f86028ed131fb5bae7f8
eddu <azabacheeduardo85@gmail.com>
2026-05-24 08:48:45 -0500

    feat: add IAM configuration for AWS Rekognition service user and access key

---
+++ iam.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_user" "rekognition_user" {
+  name = "n8n-rekognition-service-user"
+  path = "/service-accounts/"
+}
+
+resource "aws_iam_access_key" "rekognition_key" {
+  user = aws_iam_user.rekognition_user.name
+}
+
+data "aws_iam_policy" "rekognition_full_access" {
+  name = "AmazonRekognitionFullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "n8n_rekognition_attach" {
+  user       = aws_iam_user.rekognition_user.name
+  policy_arn = data.aws_iam_policy.rekognition_full_access.arn
+}
